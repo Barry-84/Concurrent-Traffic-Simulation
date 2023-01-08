e499ba0b85269b9b1bf04aafbebbe28eeaa1e7ec
Barry-84 <barrydol@gmail.com>
2023-01-08 10:36:18 +0000

    Implemented review comments

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -28,7 +28,8 @@ void MessageQueue<T>::send(T &&msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
     std::lock_guard<std::mutex> uLock(_mutex);
-    _queue.push_back(std::move(msg));
+    _queue.clear(); // prevents traffic from crossing red at peripheral intersections
+    _queue.emplace_back(std::move(msg));
     _cond.notify_one();
 }
 
@@ -98,8 +99,7 @@ void TrafficLight::cycleThroughPhases()
         {
             _currentPhase = (_currentPhase == TrafficLightPhase::green) ? TrafficLightPhase::red : TrafficLightPhase::green;
             _messageQueue.send(std::move(_currentPhase));
+            t_start = std::chrono::system_clock::now();
         }
-
-        t_start = std::chrono::system_clock::now();
     }
 }
